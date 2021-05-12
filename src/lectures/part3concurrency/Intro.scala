bb2d54d061ffc5dd527f7b580b6a4b359119bd80
guilherme-argentino <guilherme.argentino@gmail.com>
2021-05-12 12:06:25 -0300

    21. Intro to Parallel Programming on the JVM: Second run

--- src/lectures/part3concurrency/Intro.scala
+++ src/lectures/part3concurrency/Intro.scala
@@ -3,14 +3,19 @@ package lectures.part3concurrency
 object Intro extends App {
 
   /*
-    interface Runnable {
-      public void run()
-    }
+      interface Runnable {
+        public void run()
+      }
    */
   // JVM threads
-  val aThread = new Thread(() => println("Running in parallel"))
+  private val runnable = new Runnable {
+    override def run(): Unit = println("Running in parallel")
+  }
+  val aThread = new Thread(runnable)
 
   aThread.start() // gives the signal to the JVM to start a JVM thread
   // create a JVM thread => OS thread
+  runnable.run() // doesn't do anything in parallel!
+  aThread.join() // blocks until aThread finishes running
 
 }
